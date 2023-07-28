c220a8b94a15cbf13342fecdf8d93b19373acb7b
i.nekrasov <nekrulyaa@mail.ru>
2023-07-28 15:03:22 +0500

    lets gooo4

---
+++ src/components/PropertiesListItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <li v-if="item.isGroup" class="properties__item">
+    <div class="text_angle">
+      <span class="properties__item__text">{{ item.groupText }}</span>
+      <img class="angle" src="../assets/arrow-down.png" alt="open/close" />
+    </div>
+    <ul class="properties__list">
+      <PropertiesListItem
+        v-for="child in item.children"
+        :key="child.id"
+        :item="child"
+        :parentItem="item.groupText"
+      ></PropertiesListItem>
+    </ul>
+  </li>
+  <li v-if="!item.isGroup" class="properties__item">
+    <ModelPropertiesItem
+      :item="item.element"
+      :parentItem="parentItem"
+    ></ModelPropertiesItem>
+  </li>
+</template>
+
+<script>
+import ModelPropertiesItem from "./ModelPropertiesItem.vue";
+
+export default {
+  props: ["item", "parentItem"],
+  components: { ModelPropertiesItem },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.angle {
+  height: 25px;
+  position: relative;
+  top: -3px;
+}
+.text_angle {
+  display: flex;
+  justify-content: space-between;
+  max-width: fit-content;
+}
+.properties__list {
+  color: #000;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+</style>
